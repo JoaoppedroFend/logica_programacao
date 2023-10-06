523cf7b7787605207a0a01bf45c82db026751eb0
João Pedro <jpbarrosleite@gmail.com>
2023-10-05 22:14:13 -0300

    FEAT_desenvolvimento_JavaScript

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-// Lógica de programação
-// Poblema: obter 3 notas de um aluno, calcular a média
-//exibir a média e exibir a situação do aluno, aprovado ou reprovado
+function mediaAluno() {
 
-console.log('Inicio do programa');
+    // Lógica de programação
+    // Poblema: obter 3 notas de um aluno, calcular a média
+    //exibir a média e exibir a situação do aluno, aprovado ou reprovado
 
-//declaração de variaveis que não variam = cost
-//variaveis que variam = let
+    console.log('Inicio do programa');
 
-const nota1 = parseInt(prompt('Digite a nota 1:'));
-const nota2 = parseInt(prompt('Digite a nota 2:'))
-const nota3 = parseInt(prompt('Digite a nota 3:'));
+    //declaração de variaveis que não variam = cost
+    //variaveis que variam = let
+
+    const nota1 = parseInt(document.getElementById('nota1').value);
+    const nota2 = parseInt(document.getElementById('nota2').value);
+    const nota3 = parseInt(document.getElementById('nota3').value);
 
 
-//Lógica da média
-media = (nota1 + nota2 + nota3) / 3;
+    //Lógica da média
+    media = (nota1 + nota2 + nota3) / 3;
 
-console.log('A nota 1 é:' , nota1);
-console.log('A nota 2 é:' , nota2);
-console.log('A nota 3 é:' , nota3);
+    console.log('A nota 1 é:' , nota1);
+    console.log('A nota 2 é:' , nota2);
+    console.log('A nota 3 é:' , nota3);
 
-console.log('A média do aluno é:' , media);
-console.log('Fim do programa')
+    console.log('A média do aluno é:' , media);
+
+    if( media >= 7) {
+        document.getElementById('resultado').innerHTML += 'Situação: APROVADO';
+        
+    }
+    else if(media >=5 ) {
+        document.getElementById('resultado').innerHTML += 'Situação: EXAME';
+
+    
+    }
+
+    else  {
+        document.getElementById('resultado').innerHTML += 'Situação: REPROVADO';
+    
+    }
+
+    document.getElementById('resultado').innerHTML = 'A média do aluno é: ' + media + '<br>';
+
+    console.log('Fim do programa')
+
+}
